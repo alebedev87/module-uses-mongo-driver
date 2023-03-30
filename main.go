42ffd62fc1093f352e0bf46ee6302ec379e9debe
Andrey Lebedev <alebedev@redhat.com>
2023-03-30 20:29:16 +0200

    output without mongo-driver dependency

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/alebedev87/module-uses-mongo-driver/pkg/output"
+	nodeps "github.com/alebedev87/module-uses-mongo-driver/pkg/outputnodeps"
 )
 
 func main() {
 	output.Output()
+	nodeps.Output()
 }
